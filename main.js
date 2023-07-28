0a16c82ebf12a9238f78574e5d18742e94341cc8
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-07-28 11:35:47 -0300

    add: testing1

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ import ProductManager from "./class/ProductManager.js";
 import Product from "./class/Product.js";
 
 const productManager = new ProductManager();
-const product1 = new Product("producto prueba","Este es un producto prueba", 200, "Sin imagen", "abc123", 25 )
+const product1 = new Product("producto prueba","Este es un producto prueba", 200, "Sin imagen", "abc123", 25 );
+const product2 = new Product("producto prueba","Este es un producto prueba", 200, "Sin imagen", "abc123", 25 );
 
 console.log(productManager.getProducts());
 
 productManager.addProducts(product1)
+
+console.log(productManager.getProducts());
+
+productManager.addProducts(product2);
 
 console.log(productManager.getProducts());
