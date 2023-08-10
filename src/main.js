6a46d13c83f43a0c1986a97e348decd044b8f584
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-08-10 11:19:52 -0300

    add: products ok

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import Product from "./classes/Product.js";
 const product1 = new Product("producto prueba 1","Este es un producto prueba 1", 200, "Sin imagen", "abc123a", 25 );
 const product2 = new Product("producto prueba 2","Este es un producto prueba 2", 300, "Sin imagen", "abc123b", 26 );
 const product3 = new Product("producto prueba 3","Este es un producto prueba 3", 400, "Sin imagen", "abc123c", 27 );
+const product4 = new Product("producto prueba 4","Este es un producto prueba 4", 500, "Sin imagen", "abc123d", 29 );
+const product5 = new Product("producto prueba 5","Este es un producto prueba 5", 600, "Sin imagen", "abc123e", 22 );
+const product6 = new Product("producto prueba 6","Este es un producto prueba 6", 700, "Sin imagen", "abc123f", 32 );
 
 const productManager = new ProductManager();
 
 // productManager.addProduct(product1);
 // productManager.addProduct(product2);
 // productManager.addProduct(product3);
+// productManager.addProduct(product4);
+// productManager.addProduct(product5);
+// productManager.addProduct(product6);
 
 productManager.getProducts();
 // productManager.getProductById(1);
