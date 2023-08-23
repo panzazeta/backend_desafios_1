333295fe43849707f24ca2ee304f401e21579f6e
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-08-23 11:08:13 -0300

    public ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import prodsRouter from "./routes/products.routes.js";
 import cartsRouter from "./routes/cart.routes.js"
+import { __dirname } from "./path.js";
+import path from "path";
 
 const app = express();
 const PORT = 8080;
@@ -12,6 +14,10 @@ app.use(express.urlencoded({ extended: true}));
 //Routes:
 app.use("/api/products", prodsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/static", express.static(path.join(__dirname, "/public")));
+
+// console.log(__dirname + "/public");
+// console.log(path.join(__dirname, "/public"));
 
 app.listen(PORT, () => {
     console.log(`Server on port:${PORT}`)
