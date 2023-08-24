0a73b3a26fc4bc20e499b0016e93d4d9a1b056aa
panzazeta <76927307+panzazeta@users.noreply.github.com>
2023-08-24 15:03:27 -0300

    add coso

--- src/public/js/script.js
+++
@@ -1,9 +0,0 @@
-// Swal.fire({
-//     icon: 'error',
-//     title: 'Oops...',
-//     text: 'Something went wrong!',
-//     footer: '<a href="">Why do I have this issue?</a>'
-//   })
-
-const socket = io();
-socket.emit("messageConnection", "hola mr socket")
